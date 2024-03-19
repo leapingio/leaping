8ac0a7186be28460106d37022a5d6b961d875ff3
Kanav Petkar <k.petkar21@gmail.com>
2024-03-19 00:08:43 -0700

    bump versions

--- leaping/setup.py
+++ leaping/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="leaping",
-    version="0.1.8",
+    version="0.1.11",
     entry_points={
         'console_scripts': [
             'leaping=leaping:main',
@@ -10,7 +10,7 @@ setup(
     },
     python_requires='>=3.0',
     install_requires=[
-        "pytest-leaping==0.1.7",
+        "pytest-leaping==0.1.11",
         "prompt_toolkit==3.0.20",
         "openai==1.12.0"
     ],
